adc8881b0b197124b757cbfbc684908a33538228
Ioseliani <tak.ioseliani@gmail.com>
2021-05-31 19:40:59 +0400

    add bug bounty and support pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,121 +1,136 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Tokenomics from '../views/Tokenomics.vue'
-import Terms from '../views/Terms.vue'
-import Technology from '../views/Technology.vue'
-import Guideline from '../views/Guideline.vue'
-import Roadmap from '../views/Roadmap.vue'
-import Privacy from '../views/Privacy.vue'
-import Proposals from '../views/Proposals.vue'
-import Community from '../views/Community.vue'
-import Wiki from '../views/Wiki.vue'
-import ResearchAreas from '../views/ResearchAreas.vue'
-import Blog from '../views/Blog.vue'
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Tokenomics from "../views/Tokenomics.vue";
+import Terms from "../views/Terms.vue";
+import Technology from "../views/Technology.vue";
+import Guideline from "../views/Guideline.vue";
+import Roadmap from "../views/Roadmap.vue";
+import Privacy from "../views/Privacy.vue";
+import Proposals from "../views/Proposals.vue";
+import Community from "../views/Community.vue";
+import Wiki from "../views/Wiki.vue";
+import ResearchAreas from "../views/ResearchAreas.vue";
+import Blog from "../views/Blog.vue";
+import BugBounty from "../views/BugBounty.vue";
+import Support from "../views/Support.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     component: About,
     meta: { scrollToTop: true }
   },
   {
-    path: '/research-areas',
-    name: 'Researchareas',
+    path: "/research-areas",
+    name: "Researchareas",
     component: ResearchAreas,
     meta: { scrollToTop: true }
   },
   {
-    path: '/tokenomics',
-    name: 'Tokenomics',
+    path: "/tokenomics",
+    name: "Tokenomics",
     component: Tokenomics,
     meta: { scrollToTop: true }
   },
   {
-    path: '/termsofuse',
-    name: 'Termsofuse',
+    path: "/termsofuse",
+    name: "Termsofuse",
     component: Terms,
     meta: { scrollToTop: true }
   },
   {
-    path: '/technology',
-    name: 'Technology',
+    path: "/technology",
+    name: "Technology",
     component: Technology,
     meta: { scrollToTop: true }
   },
   {
-    path: '/guidelines',
-    name: 'Guidelines',
+    path: "/guidelines",
+    name: "Guidelines",
     component: Guideline,
     meta: { scrollToTop: true }
   },
   {
-    path: '/roadmap',
-    name: 'Roadmap',
+    path: "/roadmap",
+    name: "Roadmap",
     component: Roadmap,
     meta: { scrollToTop: true }
   },
   {
-    path: '/privacy',
-    name: 'Privacy',
+    path: "/privacy",
+    name: "Privacy",
     component: Privacy,
     meta: { scrollToTop: true }
   },
   {
-    path: '/proposals',
-    name: 'Proposals',
+    path: "/proposals",
+    name: "Proposals",
     component: Proposals,
     meta: { scrollToTop: true }
   },
   {
-    path: '/community',
-    name: 'Community',
+    path: "/community",
+    name: "Community",
     component: Community,
     meta: { scrollToTop: true }
   },
   {
-    path: '/wiki',
+    path: "/wiki",
     component: Wiki,
-    name: 'wikiOut',
+    name: "wikiOut",
     children: [
-        {
-            path: ':sectionName',
-            name: 'wiki',
-            component: Wiki
-        }
+      {
+        path: ":sectionName",
+        name: "wiki",
+        component: Wiki
+      }
     ],
     meta: { scrollToTop: true }
   },
   {
-    path: '/blog',
-    name: 'Blog',
+    path: "/blog",
+    name: "Blog",
     component: Blog,
     meta: { scrollToTop: true }
   },
-]
+  {
+    path: "/bug-bounty",
+    name: "BugBounty",
+    component: BugBounty,
+    meta: { scrollToTop: true }
+  },
+  {
+    path: "/support",
+    name: "Support",
+    component: Support,
+    meta: { scrollToTop: true }
+  }
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  scrollBehavior (to, from, savedPosition) {
+  scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
       return {
         selector: to.hash,
         offset: { x: 0, y: 0 }
-      }
+      };
     } else {
-      return { x: 0, y: 0 }
+      return { x: 0, y: 0 };
     }
   },
   routes
-})
+});
 
-export default router
+export default router;
